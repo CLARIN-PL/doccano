0dedba5029f50d32ef47f8c92f8af3ef27aaecac
artikandri <argikartika@gmail.com>
2022-07-20 15:48:35 +0200

    fix: ip -> localhost

--- frontend/pages/auth.vue
+++ frontend/pages/auth.vue
@@ -26,7 +26,7 @@ export default Vue.extend({
   computed: {
     ssoLoginUrl() {
       const baseUrl = window.location.origin
-      const apiUrl = process.env.API_URL || 'http://127.0.0.1:8000'
+      const apiUrl = process.env.API_URL || 'http://localhost:8000'
 
       return `${apiUrl}/keycloak/login?next=${baseUrl}/projects?limit=10&offset=0`
     }
