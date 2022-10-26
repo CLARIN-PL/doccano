a227d17f633643dcd1199dd5c6b8040bf65c2a5b
Teddy Ferdinan <64476430+teddy-f-47@users.noreply.github.com>
2022-10-26 11:40:06 +0200

    feat: Add logout button in the full-screen modal

--- frontend/components/utils/RestingPeriodModal.vue
+++ frontend/components/utils/RestingPeriodModal.vue
@@ -6,18 +6,27 @@
   >
     <v-card>
       <v-card-title>
-        <p>Time to rest!</p>
+        <p>{{ $t('generic.restingMessage.title') }}</p>
       </v-card-title>
       <v-card-text>
-        <p>Thank you for your work! Please have some coffee before continuing!</p>
-        <p>You can continue annotating at: {{ endTime }}</p>
-        <p>Please refresh this page when the time to rest has ended.</p>
+        <p>{{ $t('generic.restingMessage.message1') }}</p>
+        <p>{{ $t('generic.restingMessage.message2') }} {{ endTime }}</p>
+        <p>{{ $t('generic.restingMessage.message3') }}</p>
+
+        <v-row justify="end">
+          <button class="mr-5" @click="signout">
+            <v-icon class="button-logout-icon" :style="{ display: 'inline' }">{{ mdiLogout }}</v-icon>
+            <div class="button-logout-text" :style="{ display: 'inline' }">{{ $t('user.signOut') }}</div>
+          </button>
+        </v-row>
       </v-card-text>
     </v-card>
   </v-dialog>
 </template>
 
 <script lang="ts">
+import { mdiLogout } from '@mdi/js'
+import { mapActions } from 'vuex'
 import Vue from 'vue'
 
 export default Vue.extend({
@@ -30,8 +39,28 @@ export default Vue.extend({
 
   data() {
     return {
+      mdiLogout,
       dialog: true
+    }
+  },
+
+  methods: {
+    ...mapActions('auth', ['logout']),
+    signout() {
+      this.logout()
+      this.$router.push(this.localePath('/'))
     }
   }
 })
 </script>
+
+<style>
+.button-logout-icon {
+  display: 'inline';
+}
+
+.button-logout-text:hover {
+  text-decoration: underline;
+  display: 'inline';
+}
+</style>
