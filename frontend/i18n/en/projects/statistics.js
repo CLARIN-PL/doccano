3196df4c064856aa6446ccc32bba042eb6033865
Teddy Ferdinan <64476430+teddy-f-47@users.noreply.github.com>
2022-12-16 18:43:59 +0100

    feat: Add translations for total texts annotated

--- frontend/i18n/en/projects/statistics.js
+++ frontend/i18n/en/projects/statistics.js
@@ -2,5 +2,6 @@ export default {
   statistics: 'Metrics',
   progress: ['Completed', 'Incomplete'],
   labelStats: 'Label stats',
-  userStats: 'User stats'
+  userStats: 'User stats',
+  totalTextsAnnotated : "Total number of texts that you have annotated:"
 }
--- frontend/i18n/fr/projects/statistics.js
+++ frontend/i18n/fr/projects/statistics.js
@@ -2,5 +2,6 @@ export default {
   statistics: 'Statistiques',
   progress: ['Complété', 'Incomplet'],
   labelStats: 'Étiqueter les stats',
-  userStats: 'Stats des utilisateurs'
+  userStats: 'Stats des utilisateurs',
+  totalTextsAnnotated : "Nombre total de textes que vous avez annotés:"
 }
