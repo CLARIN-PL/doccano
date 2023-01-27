f54bc47f7ccbfa94b2a533b8fe275078a360fc38
Teddy Ferdinan <64476430+teddy-f-47@users.noreply.github.com>
2023-01-27 09:42:42 +0100

    feat: Add translations for toggling progress bar

--- frontend/i18n/fr/projects/annotation/sidebar.js
+++ frontend/i18n/fr/projects/annotation/sidebar.js
@@ -1,5 +1,6 @@
 export default {
     progress: {
+        toggle: 'Basculer la barre de progression',
         title: 'Progrès',
         total: 'Textes Totaux',
         complete: 'Textes Complétés'
