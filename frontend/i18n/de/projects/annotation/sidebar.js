4cae39a65dbe3d29eac8e26a8dcc0578e04d870a
Teddy Ferdinan <64476430+teddy-f-47@users.noreply.github.com>
2023-01-27 09:42:42 +0100

    feat: Add translations for toggling progress bar

--- frontend/i18n/de/projects/annotation/sidebar.js
+++ frontend/i18n/de/projects/annotation/sidebar.js
@@ -1,5 +1,6 @@
 export default {
     progress: {
+        toggle: 'Fortschrittsbalken umschalten',
         title: 'Fortschritt',
         total: 'Gesamt Texte',
         complete: 'Abgeschlossene Texte'
--- frontend/i18n/pl/projects/annotation/sidebar.js
+++ frontend/i18n/pl/projects/annotation/sidebar.js
@@ -1,5 +1,6 @@
 export default {
     progress: {
+        toggle: 'Przełącz pasek postępu',
         title: 'Postęp',
         total: 'Razem tekstów',
         complete: 'Zakończono tekstów'
