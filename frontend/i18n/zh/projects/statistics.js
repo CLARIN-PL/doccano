fc1fa331f0cf022fc013c29fb9d80fb80ddf4588
Teddy Ferdinan <64476430+teddy-f-47@users.noreply.github.com>
2023-02-06 16:03:57 +0100

    feat: Add translations for active minutes toggle

--- frontend/i18n/zh/projects/statistics.js
+++ frontend/i18n/zh/projects/statistics.js
@@ -6,6 +6,7 @@ export default {
   totalTextsAnnotated : "您已注释的文本总数:",
   weeklyStats: {
     button: "每周统计",
+    toggleActiveMinutes: "使用有效会议记录",
     weekSelectPrompt: "--选择一个星期的范围--",
     startDate: "开始日期: ",
     endDate: "结束日期: ",
@@ -14,16 +15,16 @@ export default {
       title: "来自所有用户的每周统计",
       totalAnnotations: "本周所做的总注解:",
       totalEveningQuestionnaires: "本周所做的晚间调查问卷总数:",
-      avgTimeAnnotate: "用户每天花在注释上的平均时间 (小时):",
-      avgTimeQuestionnaire: "用户每天花在调查问卷上的平均时间 (小时):",
+      avgTimeAnnotate: "用户每天花在注释上的平均时间 (分钟):",
+      avgTimeQuestionnaire: "用户每天花在调查问卷上的平均时间 (分钟):",
       avgTimeText: "用户花在注释一个文本上的平均时间 (分钟):",
     },
     individual: {
       title: "来自个人用户的每周统计",
       totalAnnotations: "本周该用户的总注解数:",
       totalEveningQuestionnaires: "本周该用户所做的晚间调查问卷总数:",
-      avgTimeAnnotate: "用户每天花在注释上的平均时间 (小时):",
-      avgTimeQuestionnaire: "该用户每天花在调查问卷上的平均时间 (小时):",
+      avgTimeAnnotate: "用户每天花在注释上的平均时间 (分钟):",
+      avgTimeQuestionnaire: "该用户每天花在调查问卷上的平均时间 (分钟):",
       avgTimeText: "用户注释一个文本所花的平均时间 (分钟):"
     }
   }
