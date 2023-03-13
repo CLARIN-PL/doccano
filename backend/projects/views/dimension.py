3b7eba582e9b605fc3baa7d8a8c69d7f0497bdcd
haanh764 <ngohaanh764@gmail.com>
2023-03-13 16:20:33 +0100

    del: unused code

--- backend/projects/views/dimension.py
+++ backend/projects/views/dimension.py
@@ -1,21 +1,10 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
-from projects.models import ProjectDimension, DynamicDimension, DimensionMetaData
+from projects.models import DynamicDimension, DimensionMetaData
 from projects.permissions import IsProjectAdmin, IsProjectStaffAndReadOnly
-from projects.serializers import ProjectDimensionSerializer, DynamicDimensionSerializer, DimensionMetaDataSerializer
+from projects.serializers import DynamicDimensionSerializer, DimensionMetaDataSerializer
 
-
-# class ProjectDimensionList(generics.ListCreateAPIView):
-#     serializer_class = ProjectDimensionSerializer
-#     pagination_class = None
-#     permission_classes = [IsAuthenticated & (IsProjectAdmin | IsProjectStaffAndReadOnly)]
-
-#     def get_queryset(self):
-#         return ProjectDimension.objects.filter(project=self.kwargs["project_id"])
-
-#     def perform_create(self, serializer):
-#         serializer.save(project_id=self.kwargs["project_id"])
 
 class DimensionMetaDataList(generics.ListCreateAPIView):
     serializer_class = DimensionMetaDataSerializer
